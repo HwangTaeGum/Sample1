9490e8a083f5762696b7bb1ace9c64b691a538ca
Hwang Tae Gum <tmznej12345678@gmail.com>
2022-10-22 17:54:27 +0900

    first commit

---
+++ vscode/hello.java
@@ -0,0 +1,12 @@
+package vscode;
+
+
+public class hello {
+    public static void main(String[] args){
+        int a = 30;
+        int b = 10;
+        int result = a * b;
+
+        System.out.println(result);
+    }    
+}
